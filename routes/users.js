948fabc015d234107546a0e4efee3da07e789366
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2024-07-02 18:25:36 +0200

    fix again bug

--- routes/users.js
+++ routes/users.js
@@ -86,7 +86,7 @@ router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Users.findOneByIdAndDelete(req.params.id)
+    Users.findByIdAndDelete(req.params.id)
       .then((user) => {
         console.log("user successfully deleted");
         res.json(user);

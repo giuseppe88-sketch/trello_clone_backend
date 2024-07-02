a0c04c70b7607546e6753eceb012ff9d251cf045
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2024-07-02 20:00:48 +0200

    refactor cards endppoint

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { Users, List, Card } = require("./model");
-const passport = require("passport");
 const usersRouter = require("./routes/users"); // Import the users routes
-const listsRouter = require('./routes/list'); // Import the lists routes
+const listsRouter = require("./routes/list"); // Import the lists routes
+const cardsRouter = require("./routes/cards"); // Import the lists routes
 
 const uuid = require("uuid");
 
@@ -39,7 +39,8 @@ app.use(
 );
 
 app.use("/api/users", usersRouter);
-app.use('/api/lists', listsRouter);
+app.use("/api/lists", listsRouter);
+app.use("/api/cards", cardsRouter);
 
 // mongoose.connect('mongodb://localhost:27017/trelloDB', { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb+srv://giuseppeadamo908:6Wcf8B3ifec2nxGc@trelloclone.6nnmqkb.mongodb.net/?retryWrites=true&w=majority&appName=trelloclone', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -52,14 +53,13 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-
 // app.get(
-//   "/api/list",
+//   "/api/card/",
 //   passport.authenticate("jwt", { session: false }),
 //   (req, res) => {
-//     List.find()
-//       .then((list) => {
-//         res.status(200).json(list);
+//     Card.find()
+//       .then((card) => {
+//         res.status(200).json(card);
 //       })
 //       .catch((err) => {
 //         console.error(err);
@@ -69,12 +69,12 @@ app.get("/", (req, res) => {
 // );
 
 // app.get(
-//   "/api/list/:ID",
+//   "/api/card/:ID",
 //   passport.authenticate("jwt", { session: false }),
 //   (req, res) => {
-//     List.findOne({ _id: req.params.ID })
-//       .then((listId) => {
-//         res.status(200).json(listId);
+//     Card.findOne({ _id: req.params.ID })
+//       .then((CardId) => {
+//         res.status(200).json(CardId);
 //       })
 //       .catch((err) => {
 //         console.error(err);
@@ -82,170 +82,90 @@ app.get("/", (req, res) => {
 //       });
 //   }
 // );
-app.get(
-  "/api/card/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Card.find()
-      .then((card) => {
-        res.status(200).json(card);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error:" + err);
-      });
-  }
-);
 
-app.get(
-  "/api/card/:ID",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Card.findOne({ _id: req.params.ID })
-      .then((CardId) => {
-        res.status(200).json(CardId);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error:" + err);
-      });
-  }
-);
-
-// POST/PUT ENDPOINT
-
-// router.post(
-//   "/",
+// app.post(
+//   "/api/list/:listId/card",
 //   passport.authenticate("jwt", { session: false }),
 //   (req, res) => {
-//     List.find().then((list) => {
-//       const request = req.body.title;
-//       const existingList = list.find((item) => item.title === request);
-
-//       if (existingList) {
-//         return res.status(400).json({
-//           message: "There is already a list created with the title " + request,
-//         });
-//       }
-//       const newList = new List({
-//         title: req.body.title,
-//         cards: [],
-//         createdAt: new Date(),
-//         updatedAt: new Date(),
-//       });
-//       newList
-//         .save()
-//         .then((list) => {
-//           res.status(201).json(list);
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           res.status(500).send("Error: " + err);
-//         });
+//     const newCard = new Card({
+//       listId: req.params.listId,
+//       title: req.body.title,
+//       description: req.body.description,
+//       position: req.body.position,
+//       createdAt: new Date(),
+//       updatedAt: new Date(),
 //     });
+
+//     newCard
+//       .save()
+//       .then((card) => {
+//         // Add the new card's ID to the list's cards array
+//         return List.findByIdAndUpdate(
+//           req.params.listId,
+//           { $push: { cards: card._id } },
+//           { new: true }
+//         );
+//       })
+//       .then((list) => {
+//         res
+//           .status(201)
+//           .json({ message: "Card created and added to list", list });
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send("Error: " + err);
+//       });
 //   }
 // );
 
-// router.put(
-//   "/:ID",
+// app.put(
+//   "/api/card/:ID",
 //   passport.authenticate("jwt", { session: false }),
 //   (req, res) => {
-//     List.findByIdAndUpdate(
+//     Card.findByIdAndUpdate(
 //       req.params.ID,
 //       {
 //         title: req.body.title,
+//         description: req.body.description,
+//         position: req.body.position,
 //         updatedAt: new Date(),
 //       },
 //       { new: true }
 //     )
+//       .then((card) => {
+//         if (!card) {
+//           return res.status(404).send("Card not found");
+//         }
+//         res.status(200).json(card);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send("Error: " + err);
+//       });
+//   }
+// );
+
+// app.delete(
+//   "/api/list/:listId/card/:cardId",
+//   passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     List.findById(req.params.listId)
 //       .then((list) => {
 //         if (!list) {
 //           return res.status(404).send("List not found");
 //         }
-//         res.status(200).json(list);
+
+//         // Remove the card from the list
+//         list.cards = list.cards.filter(
+//           (card) => card.toString() !== req.params.cardId
+//         );
+
+//         return list.save();
 //       })
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).send("Error: " + err);
-//       });
-//   }
-// );
-
-app.post(
-  "/api/list/:listId/card",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const newCard = new Card({
-      listId: req.params.listId,
-      title: req.body.title,
-      description: req.body.description,
-      position: req.body.position,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-
-    newCard
-      .save()
-      .then((card) => {
-        // Add the new card's ID to the list's cards array
-        return List.findByIdAndUpdate(
-          req.params.listId,
-          { $push: { cards: card._id } },
-          { new: true }
-        );
-      })
-      .then((list) => {
-        res
-          .status(201)
-          .json({ message: "Card created and added to list", list });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
-
-app.put(
-  "/api/card/:ID",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Card.findByIdAndUpdate(
-      req.params.ID,
-      {
-        title: req.body.title,
-        description: req.body.description,
-        position: req.body.position,
-        updatedAt: new Date(),
-      },
-      { new: true }
-    )
-      .then((card) => {
-        if (!card) {
-          return res.status(404).send("Card not found");
-        }
-        res.status(200).json(card);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
-
-// DELETE
-// app.delete(
-//   "/api/list/:id",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     List.findByIdAndDelete(req.params.id)
-//       .then((deletedList) => {
-//         if (!deletedList) {
-//           return res.status(404).send("List not found");
-//         }
+//       .then((updatedList) => {
 //         res
 //           .status(200)
-//           .json({ message: "List deleted successfully", deletedList });
+//           .json({ message: "Card deleted successfully", updatedList });
 //       })
 //       .catch((err) => {
 //         console.error(err);
@@ -253,35 +173,6 @@ app.put(
 //       });
 //   }
 // );
-
-app.delete(
-  "/api/list/:listId/card/:cardId",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    List.findById(req.params.listId)
-      .then((list) => {
-        if (!list) {
-          return res.status(404).send("List not found");
-        }
-
-        // Remove the card from the list
-        list.cards = list.cards.filter(
-          (card) => card.toString() !== req.params.cardId
-        );
-
-        return list.save();
-      })
-      .then((updatedList) => {
-        res
-          .status(200)
-          .json({ message: "Card deleted successfully", updatedList });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));

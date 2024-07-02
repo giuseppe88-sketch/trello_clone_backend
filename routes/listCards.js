46ab02a14028c4b1051f862fdd3e3140e3f9bb18
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2024-07-02 20:28:25 +0200

    fix bug

--- routes/listCards.js
+++ routes/listCards.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const { Card } = require("../model");
+const { List, Card } = require("../model");
 
 
 

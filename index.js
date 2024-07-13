58ed8ab3ed760976689aa95bea72e2cfb8faa6d4
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2024-07-13 23:23:13 +0200

    fix bug

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const cors = require("cors");
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Your frontend's URL
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow cookies to be sent with requests
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173", // Your frontend's URL
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true, // Allow cookies to be sent with requests
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
@@ -22,15 +22,15 @@ const cardsRouter = require("./routes/cards"); // Import the lists routes
 
 const uuid = require("uuid");
 
-app.options(
-  "*",
-  cors({
-    origin: "http://localhost:5173", // Your frontend's URL
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow cookies to be sent with requests
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.options(
+//   "*",
+//   cors({
+//     origin: "http://localhost:5173", // Your frontend's URL
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true, // Allow cookies to be sent with requests
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 // app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,21 +45,21 @@ let allowedOrigins = [
   "http://localhost:5173",
 ];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn’t found on the list of allowed origins
-//         let message =
-//           "The CORS policy for this application doesn’t allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use("/api/users", usersRouter);
 app.use("/api/lists", listsRouter);
 app.use("/api/cards", cardsRouter);
